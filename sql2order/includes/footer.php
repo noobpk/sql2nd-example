d60c3b1b09b9ace6c9f28c822af6c6e1bc38665b
noobteamcommunity <lethanhphuc@gmail.com>
2019-05-27 21:50:24 +0700

    Initial Commit

---
+++ sql2order/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid bg-4 text-center">
+  <p>Create by Noobteam <a href="https://www.noobteam.xyz">www.noobteam.xyz</a></p> 
+</footer>
+<script src="assets/bootstrap/js/jquery.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
